d6db02c22e94e29fa2414c01820b2a12e549beb2
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2021-06-01 02:29:28 +0530

    Delete router-app directory

--- router-app/src/useLocalStorage.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export default function useLocalStorage(initial_value) {
-    const [value, setValue] = useState(initial_value);
-     
-    return [value, setValue];
-} 
